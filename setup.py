b265d7b48e82362a70283851f1835753e6c04630
Joneckiz <70754247+Joneckiz@users.noreply.github.com>
2022-03-03 13:20:31 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,16 +5,13 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='calculator',
-    version='0.0.3',
+    version='0.0.1',
     author='Jonas Vengalis',
     author_email='jovengalis@gmail.com',
     description='simple python calculator',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/Joneckiz/calculator',
-    project_urls = {
-        "Bug Tracker": "https://github.com/Muls/toolbox/issues"
-    },
     license='MIT',
     packages=['calculator'],
     install_requires=['requests'],
