96c15fd824f3f4db9ddd8949c5d02f57f84f19fc
Joneckiz <70754247+Joneckiz@users.noreply.github.com>
2022-03-03 13:12:15 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setuptools.setup(
     name='calculator',
     version='0.0.3',
-    author='Mike Huls',
-    author_email='mike_huls@hotmail.com',
-    description='Testing installation of Package',
+    author='Jonas Vengalis',
+    author_email='jovengalis@gmail.com',
+    description='simple python calculator',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/Joneckiz/calculator',
