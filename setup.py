24dac2d1c96f5c90018610532adc91a95224bfee
Joneckiz <70754247+Joneckiz@users.noreply.github.com>
2022-03-03 12:33:48 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+
+setuptools.setup(
+    name='calculator',
+    version='0.0.1',
+    author='Jonas Vengalis',
+    author_email='jovengalis@gmail.com',
+    description='simple python calculator packege',
+    long_description='my first python project, code of a simple calculator 
+    url='https://github.com/Joneckiz/Python-calculator/tree/main',
+    license='MIT',
+    packages=['calculator'],
+    keywords=['python', 'calculator', 'OOP', 'classes'],
+)
